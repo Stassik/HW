50ca1047d8dfb972c4fa3015da294bfa3939b3a9
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-24 15:49:29 +0300

    create and print array in program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите длинну массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+string[] CreateArray (int lengthArray)
+{
+    string[] arr = new string[lengthArray];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите значение {i+1} элемента");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+if (length < 0) Console.WriteLine("Вы ввели отрицательное число. Введите положительное целое число!.");
+else
+{
+    string[] array = CreateArray(length);
+    PrintArray(array);
+    Console.WriteLine();
+}
